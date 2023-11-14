4d65c204b9fe119a3c2df2de784230c3552f13dd
B-Werth <bastian619werth@gmail.com>
2023-11-14 15:02:01 +0100

    animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-
+import React, { useRef } from "react";
 import { api } from "~/trpc/server";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main className="h-screen">
       <Canvas>
+        <OrbitControls></OrbitControls>
         <ambientLight intensity={2}></ambientLight>
         <directionalLight position={[2, 1, 1]}></directionalLight>
         <Cube></Cube>
@@ -17,9 +18,16 @@ export default async function Home() {
 }
 
 function Cube() {
+  const mesh = useRef(null);
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += delta * 0.1;
+  });
+
   return (
-    <mesh>
+    <mesh ref={mesh}>
       <boxGeometry args={[3, 3, 3]}></boxGeometry>
+      <meshStandardMaterial color={"orange"}></meshStandardMaterial>
     </mesh>
   );
 }
